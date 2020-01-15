a529a333f6894db16b6797936fa0828115418b91
Sam Shelton <sam.shelton@soloworks.co.uk>
2020-01-15 20:32:06 +0000

    refactor: Try moving decaration

--- generic/generic.go
+++ generic/generic.go
@@ -2,10 +2,10 @@ package generic
 
 // Device represents the base control of all devices
 type Device interface {
+	GetHost() string
 	SetSource(s int, dest int)
 	GetSource() int
 	SetPower(p bool)
 	GetPower() bool
 	Init(ip string) error
-	GetHost() string
 }
